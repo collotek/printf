1b55e6fd8f70873bef706049c665f3552efd4a5f
collotek <collinskipkorir94@gmail.com>
2023-01-27 10:31:22 +0300

    debugging

--- print_int.c
+++ print_int.c
@@ -9,7 +9,7 @@
 
 int print_int(va_list args)
 {
-	int i = 0;
+	int i;
 	int count = 0;
 	int numb;
 	int my_arr[10];
@@ -30,7 +30,7 @@ int print_int(va_list args)
 	}
 
 	/*Write the contents of the array to stdout*/
-	for (i; i >= 0; i--)
+	for (i = 0; i >= 0; i--)
 	{
 		x[0] = ('0' + my_arr[i]); /*Convert int to char*/
 		count += write(1, x, 1);
